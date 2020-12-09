bc755ed3ffdd13eea909579ae7d25eed54e76ddb
Christof Jans <christofjans@gmail.com>
2020-12-09 08:13:11 -0500

    created console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blogengine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
